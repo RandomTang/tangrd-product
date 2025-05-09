51d45038eeb2f25d572e58ce154877020a8ff88f
ExplosiveBomb <854397717@qq.com>
2025-05-09 15:47:59 +0800

    add Role-based access control

--- product-service/src/main/java/com/tangrd/product/controller/ProductController.java
+++ product-service/src/main/java/com/tangrd/product/controller/ProductController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,30 +23,35 @@ public class ProductController {
   }
 
   @PostMapping("/search")
+  @PreAuthorize("hasAuthority('SCOPE_product.read')")
   public ResponseEntity<PageResponse<ProductDTO>> searchproduct(@RequestBody ProductSearchRequest request) {
     PageResponse<ProductDTO> pageResponse = ProductService.searchProduct(request);
     return new ResponseEntity<>(pageResponse, HttpStatus.OK);
   }
 
   @GetMapping("/{id}")
+  @PreAuthorize("hasAuthority('SCOPE_product.read')")
   public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
     ProductDTO productDTO = ProductService.getProductById(id);
     return new ResponseEntity<>(productDTO, HttpStatus.OK);
   }
 
   @PostMapping
+  @PreAuthorize("hasAuthority('SCOPE_product.write')")
   public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO productDTO) {
     ProductDTO createdProduct = ProductService.createProduct(productDTO);
     return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
   }
 
   @PutMapping("/{id}")
+  @PreAuthorize("hasAuthority('SCOPE_product.write')")
   public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductDTO productDTO) {
     ProductDTO updatedProduct = ProductService.updateProduct(id, productDTO);
     return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
   }
 
   @DeleteMapping("/{id}")
+  @PreAuthorize("hasAuthority('SCOPE_product.write')")
   public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
     ProductService.deleteProduct(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- product-service/src/main/java/com/tangrd/product/exception/GlobalExceptionHandler.java
+++ product-service/src/main/java/com/tangrd/product/exception/GlobalExceptionHandler.java
@@ -1,7 +1,13 @@
 package com.tangrd.product.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +17,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -51,68 +58,42 @@ public class GlobalExceptionHandler {
     );
     return new ResponseEntity<>(validationErrorResponse, HttpStatus.BAD_REQUEST);
   }
+//
+//  @ExceptionHandler(AccessDeniedException.class)
+//  public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException ex) {
+//    ErrorResponse errorResponse = new ErrorResponse(
+//      HttpStatus.FORBIDDEN.value(),
+//      "Access denied",
+//      LocalDateTime.now()
+//    );
+//    return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+//  }
+//
+//  @ExceptionHandler(Exception.class)
+//  public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
+//    ErrorResponse errorResponse = new ErrorResponse(
+//      HttpStatus.INTERNAL_SERVER_ERROR.value(),
+//      "Internal server error",
+//      LocalDateTime.now()
+//    );
+//    return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+//  }
 
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
-    ErrorResponse errorResponse = new ErrorResponse(
-      HttpStatus.INTERNAL_SERVER_ERROR.value(),
-      "Internal server error",
-      LocalDateTime.now()
-    );
-    return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
-
-  // Error response class
+  @Data
+  @AllArgsConstructor
   static class ErrorResponse {
     private int status;
     private String message;
     private LocalDateTime timestamp;
-
-    public ErrorResponse(int status, String message, LocalDateTime timestamp) {
-      this.status = status;
-      this.message = message;
-      this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-      return status;
-    }
-
-    public void setStatus(int status) {
-      this.status = status;
-    }
-
-    public String getMessage() {
-      return message;
-    }
-
-    public void setMessage(String message) {
-      this.message = message;
-    }
-
-    public LocalDateTime getTimestamp() {
-      return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-      this.timestamp = timestamp;
-    }
   }
 
-  // Validation error response class
+  @Getter
+  @Setter
   static class ValidationErrorResponse extends ErrorResponse {
     private Map<String, String> errors;
 
     public ValidationErrorResponse(int status, String message, LocalDateTime timestamp, Map<String, String> errors) {
       super(status, message, timestamp);
-      this.errors = errors;
-    }
-
-    public Map<String, String> getErrors() {
-      return errors;
-    }
-
-    public void setErrors(Map<String, String> errors) {
       this.errors = errors;
     }
   }
